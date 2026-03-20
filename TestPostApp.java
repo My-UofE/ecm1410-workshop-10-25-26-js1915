8197b95aa27987ee522e839df5a9505d9abeb8e5
js1915 <js1915@exeter.ac.uk>
2026-03-20 12:47:29 +0000

    Task 2

---
+++ TestPostApp.java
@@ -0,0 +1,9 @@
+public class TestPostApp {
+    public static void main(String[] args) {
+        
+        Post javaPost = new Post("Alex Adam", "Help with Java", "Hi, could anyone help me I need to learn how to code in java!");
+        System.out.println(javaPost.toString());
+
+
+    }
+}
